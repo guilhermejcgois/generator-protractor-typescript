af885c7fd93c379f0cb2cc64a6788ecce566a3f5
Gois <guijocargo@gmail.com>
2017-11-20 22:15:27 -0200

    Fix lint issues

--- generators/auth0/index.js
+++ generators/auth0/index.js
@@ -6,26 +6,34 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fantastic ' + chalk.red('generator-protractor-typescript') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        'Welcome to the fantastic ' +
+          chalk.red('generator-protractor-typescript') +
+          ' generator!'
+      )
+    );
 
-    const prompts = [{
-      type: 'input',
-      name: 'className',
-      message: 'Would you like to change the class name?',
-      default: 'Auth0Page'
-    }, {
-      type: 'input',
-      name: 'fileName',
-      message: 'Would you like to change the file name?',
-      default: 'auth0.po'
-    }, {
-      type: 'input',
-      name: 'location',
-      message: 'Would you like to change the file location?',
-      default: 'e2e/'
-    }];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'className',
+        message: 'Would you like to change the class name?',
+        default: 'Auth0Page'
+      },
+      {
+        type: 'input',
+        name: 'fileName',
+        message: 'Would you like to change the file name?',
+        default: 'auth0.po'
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Would you like to change the file location?',
+        default: 'e2e/'
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
--- generators/po/index.js
+++ generators/po/index.js
@@ -6,77 +6,90 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fantastic ' + chalk.red('generator-protractor-typescript') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        'Welcome to the fantastic ' +
+          chalk.red('generator-protractor-typescript') +
+          ' generator!'
+      )
+    );
 
     const prompts = {
-      once: [{
-        type: 'input',
-        name: 'className',
-        message: 'What is the class name?',
-        default: 'AppPage'
-      }, {
-        type: 'input',
-        name: 'fileName',
-        message: 'What is the file name?',
-        default: 'app'
-      }, {
-        type: 'input',
-        name: 'location',
-        message: 'Would you like to change the file location?',
-        default: 'e2e/app/'
-      }, {
-        type: 'confirm',
-        name: 'addElement',
-        message: 'Would you like to add an element to page object?',
-        default: false
-      }],
-      loop: [{
-        type: 'input',
-        name: 'elementName',
-        message: 'What is the element name?',
-        default: 'ElementName'
-      }, {
-        type: 'confirm',
-        name: 'elementUniqueness',
-        message: 'Is the element unique on the page?',
-        default: true
-      }, {
-        type: 'list',
-        name: 'selectorType',
-        message: 'How you pretend to query element?',
-        choices: [
-          'by css',
-          'by link text'
-        ],
-        default: 'by css'
-      }, {
-        type: 'input',
-        name: 'elementSelector',
-        message: 'Which is the element selector?'
-      }, {
-        type: 'confirm',
-        name: 'oneMore',
-        message: 'Would you like to add another element to page object?',
-        default: false
-      }]
+      once: [
+        {
+          type: 'input',
+          name: 'className',
+          message: 'What is the class name?',
+          default: 'AppPage'
+        },
+        {
+          type: 'input',
+          name: 'fileName',
+          message: 'What is the file name?',
+          default: 'app'
+        },
+        {
+          type: 'input',
+          name: 'location',
+          message: 'Would you like to change the file location?',
+          default: 'e2e/app/'
+        },
+        {
+          type: 'confirm',
+          name: 'addElement',
+          message: 'Would you like to add an element to page object?',
+          default: false
+        }
+      ],
+      loop: [
+        {
+          type: 'input',
+          name: 'elementName',
+          message: 'What is the element name?',
+          default: 'ElementName'
+        },
+        {
+          type: 'confirm',
+          name: 'elementUniqueness',
+          message: 'Is the element unique on the page?',
+          default: true
+        },
+        {
+          type: 'list',
+          name: 'selectorType',
+          message: 'How you pretend to query element?',
+          choices: ['by css', 'by link text'],
+          default: 'by css'
+        },
+        {
+          type: 'input',
+          name: 'elementSelector',
+          message: 'Which is the element selector?'
+        },
+        {
+          type: 'confirm',
+          name: 'oneMore',
+          message: 'Would you like to add another element to page object?',
+          default: false
+        }
+      ]
     };
 
     this.props = {
       file: {},
       elementsInfo: []
-    }
+    };
 
     return this.prompt(prompts.once).then(props => {
       // To access props later use this.props.someAnswer;
       this.props.file = props;
-      if (!props.addElement) { return; }
+      if (!props.addElement) {
+        return;
+      }
 
       const loopPrompt = this.prompt(prompts.loop);
       const loopThen = props => {
         const oneMorePrompt = props.oneMore;
-        // delete props.oneMore;
         this.props.elementsInfo.push(props);
         if (oneMorePrompt) {
           return this.prompt(prompts.loop).then(loopThen);
@@ -87,8 +100,6 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    // const AddProductLinkText = 'Adicionar produto';
-    // const DashboardLinkSelector = 'a[ng-reflect-router-link="resumo"]';
     const props = this.props;
     const selectors = [];
     const declarations = [];
@@ -106,14 +117,28 @@ module.exports = class extends Generator {
       const selector = elementConst + searchByLinkText ? 'LinkText' : 'Selector';
       selectors.push(`const ${selector} = '${info.elementSelector}';`);
       declarations.push(`${elementVar}: ${elementType} = null;`);
-      constructorStatements.push(`this.${elementVar} = ${elementFindMethod}(${byMethod}(${selector}))`);
+      constructorStatements.push(
+        `this.${elementVar} = ${elementFindMethod}(${byMethod}(${selector}))`
+      );
     });
     const procReplacer = {
       process: function(content) {
-        let newContent = content.toString().split('/* Elements Selector */').join(selectors.join('\n\t'));
-        newContent = newContent.toString().split('/* Members Declaration */').join(declarations.join('\n\t'));
-        newContent = newContent.toString().split('/* Constructor Body */').join(constructorStatements.join('\n\t\t'));
-        newContent = newContent.toString().split('/* Class Name */').join(props.file.className);
+        let newContent = content
+          .toString()
+          .split('/* Elements Selector */')
+          .join(selectors.join('\n\t'));
+        newContent = newContent
+          .toString()
+          .split('/* Members Declaration */')
+          .join(declarations.join('\n\t'));
+        newContent = newContent
+          .toString()
+          .split('/* Constructor Body */')
+          .join(constructorStatements.join('\n\t\t'));
+        newContent = newContent
+          .toString()
+          .split('/* Class Name */')
+          .join(props.file.className);
         return newContent;
       }
     };
@@ -126,7 +151,6 @@ module.exports = class extends Generator {
   }
 
   install() {
-    // this.installDependencies();
     console.log(this.props);
   }
 };
